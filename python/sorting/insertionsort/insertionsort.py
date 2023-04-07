5e2262579c17c515298f9bafd08b98269399023d
hansnnn <pp.rongpan@gmail.com>
2023-04-07 08:13:22 +0700

    Remove pseudo code I forgot to delete
    I forgot it completely ;-;

--- python/sorting/insertionsort/insertionsort.py
+++ python/sorting/insertionsort/insertionsort.py
@@ -1,14 +1,3 @@
-#
-# insertionSort(array)
-#   mark first element as sorted
-#   for each unsorted element X
-#     'extract' the element X
-#     for j <- lastSortedIndex down to 0
-#       if current element j > X
-#         move sorted element to the right by 1
-#     break loop and insert X here
-# end insertionSort
-
 def insertionSort(array):
     if len(array) < 2:
         return array
