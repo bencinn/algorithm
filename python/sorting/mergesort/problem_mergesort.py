2e22340f70c4c35c6192d70e357c44e21413f8ed
hansnnn <pp.rongpan@gmail.com>
2023-04-11 13:23:25 +0700

    Adding more problem (mergesort/python)

--- python/sorting/mergesort/problem_mergesort.py
+++ python/sorting/mergesort/problem_mergesort.py
@@ -1,16 +1,70 @@
+from typing import List, Optional
 import mergesort
 
-# Problem 1: Given an array of integers, find the nth smallest element in the array.
+# Problem 1: Given an array of integers, find the
+#            nth smallest element in the array.
+
 
 def P1(arr, n):
     return mergesort.mergeSort(arr)[n-1]
 
-# Problem 2: Given an array of integers, return the array sorted in non-decreasing order.
+# Problem 2: Given an array of integers, return the
+#            array sorted in non-decreasing order.
+
 
 def P2(arr):
     return mergesort.mergeSort(arr)
 
-# Problem 3: Given two sorted arrays, merge them into a single sorted array.
+# Problem 3: Given two sorted arrays, merge them
+#            into a single sorted array.
+
 
 def P3(arr1, arr2):
     return mergesort.mergeArray(arr1, arr2, [])
+
+# Problem 4: Merge k Sorted Lists (LeetCode Hard)
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def mergeLL(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    if not l1:
+        return l2
+    if not l2:
+        return l1
+    if l1.val <= l2.val:
+        l1.next = mergeLL(l1.next, l2)
+        return l1
+    else:
+        l2.next = mergeLL(l1, l2.next)
+        return l2
+
+
+def sortLL(head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head or not head.next:
+        return head
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    mid, slow.next = slow.next, None
+    left, right = sortLL(head), sortLL(mid)
+    return mergeLL(left, right)
+
+
+def P4(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    if not lists:
+        return None
+    while len(lists) > 1:
+        new_lists = []
+        for i in range(0, len(lists), 2):
+            l1 = lists[i]
+            l2 = lists[i + 1] if i + 1 < len(lists) else None
+            merged_list = mergeLL(l1, l2)
+            new_lists.append(merged_list)
+        lists = new_lists
+    return lists[0]
--- python/sorting/mergesort/test_mergesort.py
+++ python/sorting/mergesort/test_mergesort.py
@@ -6,13 +6,15 @@ import timeit
 
 # Test the speed
 
+
 def test_sorting_functions():
     k = 100
     n = 500
     arr = [random.randint(1, 1000) for _ in range(k)]
 
     # test mergeSort()
-    mergeSort_time = timeit.timeit(lambda: mergesort.mergeSort(arr.copy()), number=n)
+    mergeSort_time = timeit.timeit(
+        lambda: mergesort.mergeSort(arr.copy()), number=n)
     merge_sorted = mergesort.mergeSort(arr.copy())
 
     # test sorted()
@@ -25,6 +27,7 @@ def test_sorting_functions():
 
 # Problem 1: Given an array of integers, find the nth smallest element in the array.
 
+
 def test_problemOne():
     assert problem.P1([3, 7, 1, 4, 8], 3) == 4
     assert problem.P1([3, 7, 1, 4, 8], 1) == 1
@@ -34,6 +37,7 @@ def test_problemOne():
 
 # Problem 2: Given an array of integers, return the array sorted in non-decreasing order.
 
+
 def test_problemTwo():
     assert problem.P2([3, 7, 1, 4, 8]) == [1, 3, 4, 7, 8]
     assert problem.P2([3, 7, 1, 4, 8, 0]) == [0, 1, 3, 4, 7, 8]
@@ -42,8 +46,34 @@ def test_problemTwo():
 
 # Problem 3: Given two sorted arrays, merge them into a single sorted array.
 
+
 def test_problemThree():
     assert problem.P3([1, 3, 5, 7], [2, 4, 6, 8]) == [1, 2, 3, 4, 5, 6, 7, 8]
     assert problem.P3([1, 3, 5], [2, 4, 6, 8, 10]) == [1, 2, 3, 4, 5, 6, 8, 10]
     assert problem.P3([1, 2], [3, 4]) == [1, 2, 3, 4]
     assert problem.P3([1, 1, 1], [1, 1, 1]) == [1, 1, 1, 1, 1, 1]
+
+
+def convertToLL(arr):
+    head = problem.ListNode()
+    curr = head
+    for val in arr:
+        curr.next = problem.ListNode(val)
+        curr = curr.next
+    return head.next
+
+
+def convertToArr(head):
+    arr = []
+    curr = head
+    while curr:
+        arr.append(curr.val)
+        curr = curr.next
+    return arr
+
+
+def test_problemFour():
+    assert convertToArr(problem.P4([convertToLL([1, 4, 5]), convertToLL([1, 3, 4]), convertToLL([2, 6])])) == [
+        1, 1, 2, 3, 4, 4, 5, 6]
+    assert convertToArr(problem.P4([])) == []
+    assert convertToArr(problem.P4([convertToLL([])])) == []
